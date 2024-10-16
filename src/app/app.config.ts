8b857e780dfe0ed626c0d77dd67f95b09b1ff5eb
pjimenez2510 <pjimenez2510@gmail.com>
2024-10-16 08:55:56 -0500

    Add table generic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
   ],
 };
